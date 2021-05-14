0fb792a923cc1be3b201147e338405971735045b
PlavEkmek <67767662+PlavEkmek@users.noreply.github.com>
2021-05-14 14:41:15 +0300

    Dashboard class

--- lib/model/dashboard.dart
+++ lib/model/dashboard.dart
@@ -1,24 +1,29 @@
-class Dashboard {
+import 'package:flutter/widgets.dart';
+
+class Dashboard extends ChangeNotifier{
   //Host name always stored with url. Default www.tqttio.com setted to connection 
   String _hostNameWithUrl = "www.tqttio.com ";
   //Shown name inside listed dashboardscreen  
-  String _dashboardName;
+  String _dashboardName="";
 
-  int _portNumber;
+  int _portNumber=1883;
 
   get dashboardName{
     return _dashboardName;
   }
   set portNumber(String port){
     this._portNumber = int.tryParse(port);
+    notifyListeners();
   }
   set dashboardName(String dashboardName){
     if(dashboardName==null) this._dashboardName = "Isimsiz otomasyon";
      else this._dashboardName = dashboardName;
+     notifyListeners();
   }
 
   set hostName(String host) {
     this._hostNameWithUrl = host;
+    notifyListeners();
   }
 
   Dashboard({String dashboardName,String hostName,String portnumber}){
