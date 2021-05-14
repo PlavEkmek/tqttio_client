a9628cb3eba7fc254b279579d0c941539e980559
PlavEkmek <67767662+PlavEkmek@users.noreply.github.com>
2021-05-14 14:45:39 +0300

    Created Mixin file for form validation

---
+++ lib/validation/dashboard_validate.dart
@@ -0,0 +1,22 @@
+class DashboardValidateonMixin {
+    String validateHostName(String value) {
+    // TODO hostname resolve yaptirilabilir.
+    if (value.length > 30) {
+      return "Maximum 30 karakter girilebilir";
+    }
+    return null;
+  }
+
+  String validateDashboardName(String value) {
+    if (value.length > 15) {
+      return "Maximum 15 karakter girilebilir";
+    }
+    return null;
+  }
+  String validatePortNumber(String value){
+    if(value.contains(RegExp(r'a-zA-Z'))) return "Port numarası harf olamaz";
+    int port = int.tryParse(value);
+    if(port<0 || port >25565) return "Geçersiz port numarası";
+    return null;
+  }
+}
