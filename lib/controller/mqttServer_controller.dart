02b3af2fce60e5b008faa63215127d10a9ac12d8
PlavEkmek <67767662+PlavEkmek@users.noreply.github.com>
2021-05-16 21:46:13 +0300

    Server controller

---
+++ lib/controller/mqttServer_controller.dart
@@ -0,0 +1,107 @@
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+class MqttServerControlleronMixin {
+
+  Future<MqttServerClient> initializeClient(MqttServerClient client) async {
+    /// Set logging on if needed, defaults to off
+    client.logging(on: false);
+
+    /// If you intend to use a keep alive you must set it here otherwise keep alive will be disabled.
+    client.keepAlivePeriod = 20;
+
+    /// The ws port for Mosquitto is 8080, for wss it is 8081
+    client.port = 1883;
+
+    client.setProtocolV311();
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = () => onDisconnected(client);
+
+    /// Add the successful connection callback
+    client.onConnected = onConnected;
+
+    /// Add a subscribed callback, there is also an unsubscribed callback if you need it.
+    /// You can add these before connection or change them dynamically after connection if
+    /// you wish. There is also an onSubscribeFail callback for failed subscriptions, these
+    /// can fail either because you have tried to subscribe to an invalid topic or the broker
+    /// rejects the subscribe request.
+    client.onSubscribed = onSubscribed;
+
+    /// Set a ping received callback if needed, called whenever a ping response(pong) is received
+    /// from the broker.
+    client.pongCallback = pong;
+  }
+
+  void mqttSubscribe(MqttServerClient client, String topic) {
+    String topicName = topic;
+    client.subscribe(topicName, MqttQos.atMostOnce);
+
+     client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final recMess = c[0].payload as MqttPublishMessage;
+      final pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+      print(
+          ' <${c[0].topic}>, payload is <-- $pt -->');
+      print('');
+    });
+  }
+
+  Future<void> mqttConnect(MqttServerClient client) async {
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password and clean session,
+    /// an example of a specific one below.
+    final connMess = MqttConnectMessage()
+        .withClientIdentifier('Mqtt_MyClientUniqueId')
+        .startClean() // Non persistent session for testing
+        .withWillQos(MqttQos.atMostOnce);
+    print('EXAMPLE::Mosquitto client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+    try {
+      await client.connect("user", "password");
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    }
+
+    /// Check we are connected
+    if (client.connectionStatus.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      /// Use status here rather than state if you also want the broker return code.
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+      client.disconnect();
+    }
+  }
+
+  /// The subscribed callback
+  void onSubscribed(String topic) {
+    print('EXAMPLE::Subscription confirmed for topic $topic');
+  }
+
+  /// The unsolicited disconnect callback
+  void onDisconnected(MqttServerClient client) {
+    print('EXAMPLE::OnDisconnected client callback - Client disconnection');
+    if (client.connectionStatus.disconnectionOrigin ==
+        MqttDisconnectionOrigin.solicited) {
+      print('EXAMPLE::OnDisconnected callback is solicited, this is correct');
+    }
+  }
+
+  /// The successful connect callback
+  void onConnected() {
+    print(
+        'EXAMPLE::OnConnected client callback - Client connection was sucessful');
+  }
+
+  /// Pong callback
+  void pong() {
+    print('EXAMPLE::Ping response client callback invoked');
+  }
+}
