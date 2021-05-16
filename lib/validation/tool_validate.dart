898b352e278ab3059b1c350ec83372d12e2d15e1
PlavEkmek <67767662+PlavEkmek@users.noreply.github.com>
2021-05-16 22:01:21 +0300

    Validation mixin for Tool class

---
+++ lib/validation/tool_validate.dart
@@ -0,0 +1,8 @@
+class ToolValidateonMixin{
+  String validateToolName(String value){
+    if(value.length>10) return "İsim 10 karakterden uzun olmamalıdır.";
+  }
+  String validateTopicName(String value){
+    if(value.length>10) return "İsim 10 karakterden uzun olmamalıdır.";
+  }
+}
