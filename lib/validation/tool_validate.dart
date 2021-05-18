adcac0f1cdb042fca68879fc653f572a4e2d184e
PlavEkmek <67767662+PlavEkmek@users.noreply.github.com>
2021-05-18 13:45:51 +0300

    Validation file

--- lib/validation/tool_validate.dart
+++ lib/validation/tool_validate.dart
@@ -3,6 +3,6 @@ class ToolValidateonMixin{
     if(value.length>10) return "İsim 10 karakterden uzun olmamalıdır.";
   }
   String validateTopicName(String value){
-    if(value.length>10) return "İsim 10 karakterden uzun olmamalıdır.";
+   
   }
 }
